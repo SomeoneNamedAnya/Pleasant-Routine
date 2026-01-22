3370033f7d063b7b157fbfbfcf3c4bf7cca88c07
Anna <annamyagkova14@gmail.com>
2026-01-23 00:44:52 +0300

    Little change

--- app/src/main/java/com/coursework/pleasantroutineui/domain/Destinations.kt
+++ app/src/main/java/com/coursework/pleasantroutineui/domain/Destinations.kt
@@ -2,5 +2,6 @@ package com.coursework.pleasantroutineui.domain
 
 enum class Destinations(val title: String){
     USER_ACCOUNT_PAGE("userAccountPage"),
+    USER_ROOM_PAGE("userRoomPage"),
     LOGIN_PAGE("loginPage")
 }
